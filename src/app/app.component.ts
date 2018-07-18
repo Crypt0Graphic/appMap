796e5f4a8faba95edf30de4bd7e450e05b951236
Crypt0Graphic <fettahzafer@hotmail.com>
2018-07-18 17:03:27 +0300

    markers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Styles } from './map-styles';
 
 
 @Component({
@@ -7,8 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title: string = 'My first AGM project';
+  title: string = 'Yeditepe Üniversitesi';
+  //draggable: boolean = false;
   lat: number = 51.678418;
   lng: number = 7.809007;
   zoom: 13;
+  styles = Styles;
+  markers = [
+    ['Rektörlük', 40.972918, 29.152632, "http://maps.google.com/mapfiles/kml/pal3/icon0.png"],
+    ['Mühendislik', 40.972517, 29.151553, "http://maps.google.com/mapfiles/kml/pal3/icon1.png"],
+    ['Hukuk', 40.971737, 29.153218, "http://maps.google.com/mapfiles/kml/pal3/icon2.png"]
+  ]
+
 }
+
+
+;
---
+++ src/app/map-styles.ts
@@ -0,0 +1,89 @@
+export let Styles = [
+        {
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#ebe3cd"
+            }
+          ]
+        },
+        {
+          "elementType": "labels",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#523735"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#f5f1e6"
+            }
+          ]
+        },
+        {
+          "featureType": "landscape.natural",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#dfd2ae"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#dfd2ae"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#93817c"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#f5f1e6"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway.controlled_access",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#e98d58"
+            }
+          ]
+        },
+        {
+          "featureType": "road.local",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#806b63"
+            }
+          ]
+        }
+      ]
+
