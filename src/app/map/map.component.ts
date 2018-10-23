a50ab641a49626d7dc86d328e5b90880d75b900c
Crypt0Graphic <fettahzafer@hotmail.com>
2018-10-23 10:37:44 +0300

    ng7

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -8,31 +8,25 @@ import { Markers } from './markers';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  //draggable: boolean = false;
-  lat: number = 40.972177;
-  lng: number = 29.152087;
-  zoom: number = 16.5;
+  // draggable: boolean = false;
+  lat = 40.972177;
+  lng = 29.152087;
+  zoom = 16.5;
 
   styles = Styles;
   markers = Markers;
 
-  path0 = [
-    { lat: 40.972373, lng: 29.151818 },
-    { lat: 40.972060, lng: 29.152510 }
-  ];
+  path0 = [{ lat: 40.972373, lng: 29.151818 }, { lat: 40.97206, lng: 29.15251 }];
 
   path1 = [
     { lat: 40.972585, lng: 29.152288 },
-    { lat: 40.972060, lng: 29.152510 },
+    { lat: 40.97206, lng: 29.15251 },
     { lat: 40.971875, lng: 29.152733 },
-    { lat: 40.971760, lng: 29.152693 },
-    { lat: 40.971742, lng: 29.152727}
+    { lat: 40.97176, lng: 29.152693 },
+    { lat: 40.971742, lng: 29.152727 }
   ];
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
